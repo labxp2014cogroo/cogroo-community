c48e004e8d8c7636b0ccbdbb3cf4116925400b9f
Ricardo F. Guimarães <rfguimaraes0@gmail.com>
2014-03-21 16:09:19 -0300

    Piloto: Vinicius; Co-piloto: Ricardo; Co-piloto: Vitor --> Tentando obter o JASON do webservice.

---
+++ src/main/java/br/usp/ime/cogroo/controller/WordController.java
@@ -0,0 +1,58 @@
+package br.usp.ime.cogroo.controller;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+
+@Resource
+public class WordController {
+
+	private final Result result;
+
+	public WordController(Result result) {
+		this.result = result;
+	}
+
+	@Path("/newEntry")
+	public void newEntry() {
+	}
+
+	@Path("/dictionaryEntrySearch")
+	public void dictionaryEntrySearch() {
+	}
+
+	@Post
+	@Path("/searchEntry")
+	public void searchEntry(String text) {
+
+		result.include("text", text);
+		result.redirectTo(getClass()).dictionaryEntrySearch();
+	}
+	
+	private JSONObject nomeDisso(String text) throws ClientProtocolException, IOException {
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet("http://200.18.49.108:4040/query.json?palavra=" + text);
+        HttpResponse response = client.execute(request);
+        Scanner scanner = new Scanner(response.getEntity().getContent());
+        try {
+			JSONObject wordDescriptor = new JSONObject(scanner.nextLine());
+			return wordDescriptor;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+        return null;
+	}
+
+}
