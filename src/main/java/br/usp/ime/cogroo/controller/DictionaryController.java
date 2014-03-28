8fef9c41800aeb7335d57167cbd47a25316662df
Rodrigo Duarte Louro <digao.louro@gmail.com>
2014-03-28 08:29:30 -0300

    Escaneando a entrada do usuario Piloto:Rodrigo, Co-piloto: Arthur

--- src/main/java/br/usp/ime/cogroo/controller/DictionaryController.java
+++ src/main/java/br/usp/ime/cogroo/controller/DictionaryController.java
@@ -1,14 +1,53 @@
 package br.usp.ime.cogroo.controller;
 
+import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.usp.ime.cogroo.dao.CogrooFacade;
+import br.usp.ime.cogroo.logic.TextSanitizer;
+import br.usp.ime.cogroo.model.LoggedUser;
 
 @Resource
 public class DictionaryController {
-	
+
+	private final Result result;
+
+	public DictionaryController(Result result) {
+		this.result = result;
+	}
+
 	@Path("/newEntry")
 	public void newEntry() {
+	
+	}
+
+	@Get
+	@Path("/newEntry/{text*}")
+	public void newEntryGET(String text) {
 
 	}
 
+	@Post
+	@Path("/trataPalavra")
+	public void trataPalavra(String word) {
+		String mensagem = "";
+		if (word == null) {
+			mensagem = "ERRO!!!";
+		}
+		else if (word.length() == 0) {
+			mensagem = "Nenhuma palavra foi digitada!";
+		}
+		else {
+			//Tem que chamar o metodo de busca de palavras no jspell com word
+			mensagem = "Vc digitou: " + word;
+		}
+		result.include("mensagem", mensagem).redirectTo(this).newEntry();
+	}
 }
+
+
+
+
+
--- src/main/webapp/WEB-INF/jsp/dictionary/newEntry.jsp
+++ src/main/webapp/WEB-INF/jsp/dictionary/newEntry.jsp
@@ -1,9 +1,10 @@
-
 <h2>Inserir Palavras</h2>
 
 
 <p>Digite aqui a palavra a ser inserida no dicionário</p>
-<form action="<c:url value="/grammar"/>"  method="post" >
-    <textarea rows="1" cols="70" name="text" id="text">${text}</textarea>
-    <input type="submit" value=" OK &raquo; " id="go"/>
+<form action="<c:url value="/trataPalavra"/>"  method="post" >
+    <input type="text" name="word" id="word"><br>
+    <input type="submit" value=" OK &raquo; " id="go"/>    
 </form>
+<br />
+${mensagem}
