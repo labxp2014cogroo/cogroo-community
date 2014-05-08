a149817bb5a0051be1fdeea7390f1d677aae92fb
vitorlima <vitor_lp@yahoo.com>
2014-05-08 09:45:41 -0300

    Teste JUnit (para o TDD) e a classe Flag (#6)
    FlagsTest --> Um teste de unidade para a classe Flags
    Flags --> Classe para encapsular os textos que serão mostrados na tela
    flags.properties --> Arquivo com a relação de flags e textos.
    
    [Vitor, Arthur]

---
+++ src/test/java/br/usp/ime/cogroo/model/FlagsTest.java
@@ -0,0 +1,24 @@
+package br.usp.ime.cogroo.model;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class FlagsTest {
+
+	@Test
+	public void testGetTextFromFlag() {
+		try {
+			Flags flags = Flags.getInstance();
+			System.out.println(flags.getTextFromFlag("a"));
+			Assert.assertEquals("O plural é da forma mão -> mãos", flags.getTextFromFlag("a"));
+			Assert.assertEquals("Aceita o prefixo \"anti-\"", flags.getTextFromFlag("A"));
+//			Assert.assertEquals("Aceita", flags.getTextFromFlag("c"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
